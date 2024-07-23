fdeb203322fce711b49f047d71bfb433b5a99da2
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2024-07-22 21:05:59 -0300

    14. Como criptografar a senha do usuário com bcrypt no Express com MongoDb

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const connetcDatabase = () => {
-    console.log("Agurade, conectando ao Banco de Dados...")
+    console.log("Aguarde, conectando ao Banco de Dados...")
 
     mongoose.connect("mongodb+srv://GuilhermeRS:TxncPjjh1lWPutOI@breakingnews.bd5gs12.mongodb.net/?retryWrites=true&w=majority&appName=BreakingNews"
     )
-        .then(() => console.log("Banco de Dados conectado."))
+        .then(() => console.log("Banco MongoDB Atlas conectado."))
         .catch((error) => console.log(error));
 };
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,32 +1,39 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        require:true
+        require: true
     },
     username: {
         type: String,
-        require:true
+        require: true
     },
     email: {
         type: String,
-        require:true,
+        require: true,
         unique: true
     },
     password: {
         type: String,
-        require:true
+        require: true,
+        select: false
     },
     avatar: {
         type: String,
-        require:true
+        require: true
     },
     background: {
         type: String,
-        require:true
+        require: true
     }
 });
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
