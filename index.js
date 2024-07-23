72dd26718b9130a40925cde3074f313d3c1ee646
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2024-07-22 21:25:02 -0300

    15. Variáveis de ambiente com dotenv em um projeto Express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoute from './src/routes/user.router.js';
 import connetcDatabase from "./src/database/db.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 connetcDatabase();
 app.use(express.json());
--- src/database/db.js
+++ src/database/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const connetcDatabase = () => {
     console.log("Aguarde, conectando ao Banco de Dados...")
 
-    mongoose.connect("mongodb+srv://GuilhermeRS:TxncPjjh1lWPutOI@breakingnews.bd5gs12.mongodb.net/?retryWrites=true&w=majority&appName=BreakingNews"
-    )
+    mongoose.connect(process.env.URI_MOOGODB)
         .then(() => console.log("Banco MongoDB Atlas conectado."))
         .catch((error) => console.log(error));
 };
